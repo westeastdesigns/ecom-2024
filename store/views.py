ec38486525da3c6a702e8391186a4a75803e1fba
westeastdesigns <47990949+westeastdesigns@users.noreply.github.com>
2024-05-13 11:03:09 -0400

    added new user registration

--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from django.shortcuts import redirect, render
 
+from .forms import SignUpForm
 from .models import Product
 
 
@@ -39,3 +43,30 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out... Thanks for visiting!"))
     return redirect("home")
+
+
+def register_user(request):
+    form = SignUpForm()
+    # if they fill out the form
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            # log the user in
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have been registered... Welcome!"))
+            return redirect("home")
+        else:
+            # if the form is not valid
+            messages.success(
+                request,
+                (
+                    "Uh oh, there was an error. Please check all of your entries and try to register again."
+                ),
+            )
+            return redirect("register")
+    else:
+        return render(request, "register.html", {"form": form})
